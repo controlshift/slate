1e90f749615b4db02d07e22e1162840946d6a244
Jacinda Moore <jacinda@controlshiftlabs.com>
2018-03-16 09:40:25 -0400

    Update Bulk Data section with addition of email_opt_in_types (#12)
    * update headers for attendees, signatures, and users
    
    * Add new email_opt_in_types table to bulk data section
    
    * update schema files for signatures, attendees, and users
    
    The current set of columns is copied from the structure file, minus
    columns that are excluded from bulk exports.
    
    * remove email_opt_in_types.name

---
+++ source/data/email_opt_in_types_schema.sql
@@ -0,0 +1,18 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+
+SET search_path = public, pg_catalog;
+
+CREATE TABLE email_opt_in_types (
+    id BIGSERIAL PRIMARY KEY,
+    organisation_id bigint NOT NULL,
+    kind character varying NOT NULL,
+    mailable boolean,
+    active boolean,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
